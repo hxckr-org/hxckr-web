9fcaccf1bcb0efbef66a097ce1cc62e090d885f6
daviroo <david.borthwick@gmail.com>
2024-11-13 09:31:37 +0000

    chore: set inter font globally

--- src/app/components/layout/landing-page-layout.tsx
+++ src/app/components/layout/landing-page-layout.tsx
@@ -3,7 +3,7 @@ import Footer from "@/app/components/sections/footer";
 
 export default function LandingPageLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex text-black flex-col items-center justify-between mx-auto font-p22mackinac">
+    <main className="flex text-black flex-col items-center justify-between mx-auto font-inter">
       <div className="w-full">
         <Navbar />
         {children}
