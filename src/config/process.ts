8d4b8bef27f2d74ceba4ca94895a40e233627993
Theophilus <isahtheophilus@gmail.com>
2024-11-22 22:21:24 +0100

    chore: check env only if window is defined

--- src/config/process.ts
+++ src/config/process.ts
@@ -6,14 +6,15 @@ export const websocketUrl = process.env.NEXT_PUBLIC_APP_WEBSOCKET_URL;
 export const githubClientID = process.env.GITHUB_ID as string;
 export const githubClientSecret = process.env.GITHUB_SECRET as string;
 
-// Check only client-side variables
-if (!coreBaseUrl || !websocketUrl) {
-  // log the missing variables
-  console.log({ coreBaseUrl, websocketUrl });
-  throw new Error("Missing client-side environment variables");
+// Only run client-side checks in the browser
+if (typeof window !== "undefined") {
+  if (!coreBaseUrl || !websocketUrl) {
+    console.log({ coreBaseUrl, websocketUrl });
+    throw new Error("Missing client-side environment variables");
+  }
 }
 
-// Do not run server-side checks during the build
+// Server-side checks
 if (typeof window === "undefined") {
   if (!githubClientID || !githubClientSecret) {
     console.warn("Missing server-side environment variables for GitHub OAuth.");
