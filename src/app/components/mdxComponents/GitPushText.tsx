63f045c5a404005230508a03bcd038188fe174e4
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2024-12-01 14:40:52 +0100

    chore: fix issue with websocket logs (#67)

--- src/app/components/mdxComponents/GitPushText.tsx
+++ src/app/components/mdxComponents/GitPushText.tsx
@@ -18,35 +18,45 @@ export const GitPushText = ({
   const pathname = usePathname();
   const { messages, isConnected } = useAuthenticatedWebSocket();
   const { websocketEvents, addWebsocketEvent, allRepositories } = useStore();
+
   const [displayText, setDisplayText] = useState(content);
+  const [showCheckIcon, setShowCheckIcon] = useState(false);
 
   const currentPushEvent = websocketEvents?.pushEvents?.[moduleNumber];
 
   const challengeUrl = pathname.split("/challenges")[1];
   const document = getChallengeDocument({ url: challengeUrl });
-
-  const currentRepo = allRepositories.find(
-    (repo) =>
-      repo.soft_serve_url === currentPushEvent?.repoUrl ||
-      sluggify(repo?.challenge.title) === sluggify(document?.title || "")
-  );
+  const currentRepo = allRepositories.find((repo) => {
+    return (
+      sluggify(repo?.challenge?.title || "") === sluggify(document?.title || "")
+    );
+  });
   const currentStep = currentRepo?.progress.progress_details.current_step || 0;
 
   useEffect(() => {
-    if (currentPushEvent) {
+    if (
+      currentPushEvent &&
+      currentRepo?.soft_serve_url === currentPushEvent.repoUrl
+    ) {
       setDisplayText(
         `Success... new push detected on branch ${
           currentPushEvent?.branch
         } (${currentPushEvent?.commitSha.slice(0, 7)})`
       );
+      setShowCheckIcon(true);
     } else if (
       !currentPushEvent &&
       currentRepo?.progress.progress_details.current_step &&
       currentRepo?.progress.progress_details.current_step > moduleNumber
     ) {
       setDisplayText(`Success... git repository created and cloned`);
+      setShowCheckIcon(true);
+    } else if (currentPushEvent && currentStep <= moduleNumber) {
+      setDisplayText(`Success... git repository created and cloned`);
+      setShowCheckIcon(true);
     } else {
       setDisplayText(content);
+      setShowCheckIcon(false);
     }
   }, [currentPushEvent, currentRepo, moduleNumber, content]);
 
@@ -69,12 +79,12 @@ export const GitPushText = ({
       <div className="mt-4">
         <p
           className={`text-sm ${
-            isConnected && (currentPushEvent || currentStep > moduleNumber)
+            isConnected && showCheckIcon
               ? "flex items-center gap-x-2 text-green-primary"
               : "text-grey-tertiary-text animate-pulse"
           }`}
         >
-          {isConnected && (currentPushEvent || currentStep > moduleNumber) && (
+          {isConnected && showCheckIcon && (
             <CheckCircledIcon className="w-5 h-5 text-green-primary" />
           )}
           {displayText}
